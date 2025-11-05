e3e03cf17a342ae4f1df3bffc47464da62dc8f43
JoanaMorenoC <juana.moreno@enti.cat>
2025-11-05 12:50:13 +0100

    [CODING] Make camera a prefab

--- Prototipo2/Assets/Scripts/CameraMovement.cs
+++ Prototipo2/Assets/Scripts/CameraMovement.cs
@@ -5,23 +5,16 @@ using static UnityEngine.GraphicsBuffer;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] GameManager gameManager; // Temporal, luego se podría hacer que gamemanager sea singleton
-    [SerializeField] int distanceNeededToMove = 3;
     [SerializeField] float distanceThreshold = 0.01f;
 
     Vector3 velocity = Vector3.zero;
     float smoothTime = 0.3f;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-        if (gameManager.GetFarthestDistance() > transform.position.z)
+        if (GameManager.Instance.GetFarthestDistance() > transform.position.z)
         {
-            Vector3 targetPosition = new Vector3(0f, 0f, gameManager.GetFarthestDistance());
+            Vector3 targetPosition = new Vector3(0f, 0f, GameManager.Instance.GetFarthestDistance());
 
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
