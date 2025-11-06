706f17b6dfa604cc92b830fef7ea25e573ba022b
erica <erica.baltar@enti.cat>
2025-11-06 10:41:47 +0100

    [CODING] Map Done

--- Prototipo2/Assets/Scripts/Game Manager.cs
+++ Prototipo2/Assets/Scripts/Game Manager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
 
     [Header("Terrain objects")]
     [SerializeField] private Grass grassPrefab;
+    [SerializeField] private Road roadPrefab;
 
     [Header("Game parameters")]
     [SerializeField] private float moveDuration = 0.2f;
@@ -59,9 +60,21 @@ public class GameManager : MonoBehaviour
 
     private void SpawnObstacle()
     {
-        // Create grass with terrain height of 0.2f.
-        Grass grass = Instantiate(grassPrefab, terrainHolder);
-        obstacles.Add((0.2f, grass.Init(spawnLocation)));
+        // Spawn more roads the further we get, at 250 have 90% chance of a road.
+        float roadProbability = Mathf.Lerp(0.5f, 0.9f, spawnLocation / 250f);
+
+        if (Random.value < roadProbability)
+        {
+            // Create road with terrain height of 0.1f.
+            Road road = Instantiate(roadPrefab, terrainHolder);
+            obstacles.Add((0.1f, road.Init(spawnLocation)));
+        }
+        else
+        {
+            // Create grass with terrain height of 0.2f.
+            Grass grass = Instantiate(grassPrefab, terrainHolder);
+            obstacles.Add((0.2f, grass.Init(spawnLocation)));
+        }
 
         // Update to the next free location
         spawnLocation++;
@@ -145,6 +158,10 @@ public class GameManager : MonoBehaviour
 
         // The yHeight changes if we're on grass or road.
         float yHeight = 0.2f;
+        if (characterPos.y >= 0)
+        {
+            yHeight = obstacles[characterPos.y].terrainHeight;
+        }
 
         Vector3 startPos = character.position;
         Vector3 endPos = new(characterPos.x, yHeight, characterPos.y);
